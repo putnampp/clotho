3bba05fdef7ed51bf012a9e5e30ad2a0ca1c6447
Patrick Putnam <putnampp@gmail.com>
2014-09-17 13:37:41 -0400

    Initial commit

---
+++ include/clotho/recombination/recombination.hpp
@@ -0,0 +1,7 @@
+#ifndef RECOMBINATION_GUARD_HPP_
+#define RECOMBINATION_GUARD_HPP_
+
+#include "clotho/recombination/recombine_bitset.hpp"
+//#include "clotho/recombination/recombine_bitset_sorted.tcc"
+
+#endif  // RECOMBINATION_GUARD_HPP_
