848266294dd84e0c8c05a99f3d6c5507b4201506
Patrick Putnam <putnampp@gmail.com>
2014-09-24 12:13:17 -0400

    Initial commit

---
+++ src/examples/common_strings.cpp
@@ -0,0 +1,6 @@
+#include "common_strings.h"
+
+const string UNKNOWN_K = "unknown";
+const string MALE_K = "male";
+const string FEMALE_K = "female";
+const string UNASSIGNED_K = "unassigned";
---
+++ src/examples/common_strings.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_STRINGS_H_
+#define COMMON_STRINGS_H_
+
+#include <string>
+using std::string;
+
+extern const string UNKNOWN_K;
+
+extern const string MALE_K;
+extern const string FEMALE_K;
+extern const string UNASSIGNED_K;
+
+#endif  // COMMON_STRINGS_H_
