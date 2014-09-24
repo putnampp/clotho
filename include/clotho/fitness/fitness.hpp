98d5b5c542de0b7e6f3d3323461dc98491deba80
Patrick Putnam <putnampp@gmail.com>
2014-09-24 12:30:38 -0400

    Initial definition of fitness; Using boost libraries like bimap as an example of how to organize such functionality

--- include/clotho/fitness/fitness.hpp
+++ include/clotho/fitness/fitness.hpp
@@ -1,6 +1,27 @@
 #ifndef FITNESS_GUARD_HPP_
 #define FITNESS_GUARD_HPP_
 
-#include "clotho/fitness/fitness_bitset.hpp"
+//#include "clotho/fitness/fitness_bitset.hpp"
+//
+
+#include "clotho/fitness/detail/fitness_core.hpp"
+
+template < class Result, class Individual, class SelectionTag >
+class fitness :
+public clotho::fitness::detail::fitness_core< Result, Individual, SelectionTag >,
+public clotho::fitness::detail::fitness_core< Result, Individual, SelectionTag >::evaluation_type
+{
+public:
+    
+    typedef clotho::fitness::detail::fitness_core< Result, Individual, SelectionTag > core;
+    typedef core::evaluation_type eval_type;
+
+    typedef eval_type::hom_policy_type  hom_policy_type;
+    typedef eval_type::het_policy_type  het_policy_type;
+
+    fitness( const hom_policy_type & hom, const het_policy_type & het ) : eval_type( hom, het ) {}
+
+};
+
 
 #endif  // FITNESS_GUARD_HPP_
