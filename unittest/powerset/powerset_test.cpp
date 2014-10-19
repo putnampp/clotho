a709f70768ffa65d4791e522c89f6158e9238911
Patrick Putnam <putnampp@gmail.com>
2014-10-19 11:44:03 -0400

    added basic unit tests

--- unittest/powerset/powerset_test.cpp
+++ unittest/powerset/powerset_test.cpp
@@ -39,7 +39,7 @@ struct block_map< test_element, Block > {
     static const unsigned int bits_per_block = sizeof(size_type) * 8;
 
     inline size_type operator()( const element_type & elem ) {
-        assert( 0. < elem.k && elem.k < 1. );
+        assert( 0. <= elem.k && elem.k < 1. );
 
         return elem.k * bits_per_block;
     }
@@ -50,21 +50,39 @@ struct block_map< test_element, Block > {
 
 typedef clotho::powersets::variable_subset< test_element > subset_type;
 typedef typename subset_type::powerset_type powerset_type;
+typedef powerset_type::block_map_type bmap;
 
 BOOST_AUTO_TEST_SUITE( test_powerset )
 
-BOOST_AUTO_TEST_CASE( create_powerset ) {
+BOOST_AUTO_TEST_CASE( create_powerset_simple) {
 
     powerset_type ps;
 
-    typename powerset_type::block_map_type bmap;
-    
-    double k = 0.5;
+    double k = 0.75;
     test_element te( k, 2.0 );
 
     typename powerset_type::element_index_type idx = ps.find_or_create(te);
+    BOOST_REQUIRE_MESSAGE( idx == bmap()(te), "Unexpected index returned" );
+    BOOST_REQUIRE_MESSAGE( ps.size() == 1, "Unexpected size" );
+    BOOST_REQUIRE_MESSAGE( ps.variable_allocated_size() == bmap::bits_per_block, "Unexpected variable space: " << ps.variable_allocated_size() );
 
-    BOOST_REQUIRE_MESSAGE( idx == bmap(te), "Unexpected index returned" );
+}
+
+BOOST_AUTO_TEST_CASE( create_powerset_width ) {
+    powerset_type ps;
+
+    BOOST_REQUIRE_MESSAGE( ps.empty(), "Unexpected size" );
+    for( unsigned int i = 0; i < bmap::bits_per_block; ++i ) {
+        double v = (double) i;
+        double k = v / (double) bmap::bits_per_block;
+        test_element te(k, 1.0);
+
+        typename powerset_type::element_index_type idx = ps.find_or_create(te);
+
+        BOOST_REQUIRE_MESSAGE( idx == i, "Unexpected index " << idx << " returned for " << i << "(" << k << ")" );
+    }
+
+    BOOST_REQUIRE_MESSAGE( ps.size() == bmap::bits_per_block, "Unexpected size" );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
