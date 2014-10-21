20c3d3f186b7dad4b1e6b1f3246a3f227972eef8
Patrick Putnam <putnampp@gmail.com>
2014-10-21 14:59:21 -0400

    Default ordering of elements in powerset

---
+++ include/clotho/powerset/next_available_order.hpp
@@ -0,0 +1,12 @@
+#ifndef CLOTHO_NEXT_AVAILABLE_ORDER_HPP_
+#define CLOTHO_NEXT_AVAILABLE_ORDER_HPP_
+
+namespace clotho {
+namespace powersets {
+
+struct next_available_order { };
+
+}   // namespace powersets
+}   // namespace clotho
+
+#endif  // CLOTHO_NEXT_AVAILABLE_ORDER_HPP_
