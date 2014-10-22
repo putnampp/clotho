960d6b4b3b060dd09b5503db0001be31b07136f3
Patrick Putnam <putnampp@gmail.com>
2014-10-22 11:10:10 -0400

    rename

---
+++ include/clotho/powerset/user_defined_tag.hpp
@@ -0,0 +1,12 @@
+#ifndef CLOTHO_USER_DEFINED_TAG_HPP_
+#define CLOTHO_USER_DEFINED_TAG_HPP_
+
+namespace clotho {
+namespace powersets {
+
+struct user_defined_tag {};
+
+}   // namespace powersets
+}   // namespace clotho
+
+#endif  // CLOTHO_USER_DEFINED_TAG_HPP_
