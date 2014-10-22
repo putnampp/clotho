4f069d7200831678e6304f5c485ecf04b82dbb03
Patrick Putnam <putnampp@gmail.com>
2014-10-22 11:12:39 -0400

    Ability to specify a static range within which all key values will exist

---
+++ include/clotho/powerset/key_range.hpp
@@ -0,0 +1,20 @@
+#ifndef KEY_RANGE_HPP_
+#define KEY_RANGE_HPP_
+
+namespace clotho {
+namespace powersets {
+
+struct normal_01 {};
+
+template < class R = normal_01 >
+struct key_range {
+    static constexpr double min = 0.0;
+    static constexpr double max = 1.0;
+
+    static constexpr double range = (max - min);
+};
+
+}   // namespace powersets
+}   // namespace clotho
+
+#endif  // KEY_RANGE_HPP_
