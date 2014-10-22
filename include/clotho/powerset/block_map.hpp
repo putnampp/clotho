a73c9a0ef66ba460ef705e21ad08a1411dfb01dd
Patrick Putnam <putnampp@gmail.com>
2014-10-22 11:18:27 -0400

    Fixed tag name after refactor; Added partial specialization for normalized keys

--- include/clotho/powerset/block_map.hpp
+++ include/clotho/powerset/block_map.hpp
@@ -1,25 +1,38 @@
 #ifndef BLOCK_MAP_HPP_
 #define BLOCK_MAP_HPP_
 
-#include "clotho/powerset/next_available_order.hpp"
+#include "clotho/powerset/normalized_key.hpp"
+
+#include "clotho/powerset/next_available_order_tag.hpp"
 
 namespace clotho {
 namespace powersets {
 
-template < class Element, class Block, class Tag = next_available_order >
+template < class Element, class Block, class Tag = next_available_order_tag >
 struct block_map {
     typedef Element element_type;
     typedef Block   size_type;
 
     static const unsigned int       bits_per_block = sizeof( size_type ) * 8;
 
-    static constexpr double         min_bin_value = 0.0;
-    static constexpr double         max_bin_value = 1.0;
-
-    constexpr static const double   width_per_bin = ((max_bin_value - min_bin_value) / (double) bits_per_block);
-
     size_type operator()( const element_type & elem ) {
         return -1;
+    }
+};
+
+template < class Element, class Block >
+struct block_map< Element, Block, normalized_key< Element > > {
+    typedef Element element_type;
+    typedef Block   size_type;
+
+    typedef normalized_key< Element > keyer;
+
+    static const unsigned int       bits_per_block = sizeof( size_type ) * 8;
+    constexpr static const double   width_per_bin = ((keyer::range) / (double) bits_per_block);
+
+    inline size_type operator()( const element_type & elem ) {
+        static keyer k;
+        return k(elem) * bits_per_block;
     }
 };
 
