a32649ba59983c07fb34ddc1fb3574768f48fa5c
Patrick Putnam <putnampp@gmail.com>
2014-10-22 11:45:07 -0400

    separated block bit mapping to bit_range since not dependent upon element

--- include/clotho/powerset/block_map.hpp
+++ include/clotho/powerset/block_map.hpp
@@ -1,6 +1,7 @@
 #ifndef BLOCK_MAP_HPP_
 #define BLOCK_MAP_HPP_
 
+#include "clotho/powerset/bit_range.hpp"
 #include "clotho/powerset/normalized_key.hpp"
 
 #include "clotho/powerset/next_available_order_tag.hpp"
@@ -21,18 +22,20 @@ struct block_map {
 };
 
 template < class Element, class Block >
-struct block_map< Element, Block, normalized_key< Element > > {
+struct block_map< Element, Block, normalized_key< Element > > : public bit_range< Block, typename normalized_key< Element >::range_type > {
     typedef Element element_type;
     typedef Block   size_type;
 
     typedef normalized_key< Element > keyer;
 
-    static const unsigned int       bits_per_block = sizeof( size_type ) * 8;
-    constexpr static const double   width_per_bin = ((keyer::range) / (double) bits_per_block);
+    typedef bit_range< Block, typename normalized_key< Element >::range_type > bit_range_type;
+
+//    static const unsigned int       bits_per_block = sizeof( size_type ) * 8;
+//    constexpr static const double   width_per_bin = ((keyer::range) / (double) bits_per_block);
 
     inline size_type operator()( const element_type & elem ) {
         static keyer k;
-        return k(elem) * bits_per_block;
+        return k(elem) * bit_range_type::bits_per_block;
     }
 };
 
