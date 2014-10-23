84cb6039c428b77eac0a02dc839543ec96e9b30b
Patrick Putnam <putnampp@gmail.com>
2014-10-23 13:38:18 -0400

    Testing type_trait specifications

--- unittest/utility/bit_walker_test.cpp
+++ unittest/utility/bit_walker_test.cpp
@@ -1,12 +1,15 @@
 #include <boost/test/unit_test.hpp>
 
 #include <vector>
+#include <type_traits>
 
 #include "clotho/utility/bit_walker.hpp"
 
 typedef unsigned long block_type;
 typedef unsigned short sub_block_type;
 typedef clotho::utility::block_walker< block_type, sub_block_type > walker_type;
+
+typedef clotho::utility::block_walker< block_type, sub_block_type, clotho::utility::bit_block_walker< sizeof(block_type) * 8> > other_walker_type;
 
 struct set_bit_vector_op {
 
@@ -70,6 +73,35 @@ BOOST_AUTO_TEST_CASE( test_bit_walker_set_bits ) {
 
     set_bit_vector_op sbv;
     walker_type::apply( _bits, sbv );
+
+    BOOST_REQUIRE_MESSAGE( sbv.indices == expected, "Unexpected list of set bits was returned" );
+}
+
+/**
+ * Initialize a block with every odd bit set, and initialize an expected index vector
+ *
+ * Apply the set_bit_vector_op
+ *
+ * Verify that the result vector matches the expected vector
+ */
+BOOST_AUTO_TEST_CASE( test_other_bit_walker_set_bits ) {
+    BOOST_REQUIRE_MESSAGE( (std::is_same< other_walker_type::bit_walker_type, clotho::utility::bit_block_walker< sizeof(block_type) * 8 > >::value), "Unexpected bit block walker (test 1)" );
+    BOOST_REQUIRE_MESSAGE( (!std::is_same< other_walker_type::bit_walker_type, clotho::utility::bit_block_walker< 16 > >::value), "Unexpected bit block walker (test 2)" );
+
+    std::vector< unsigned int > expected;
+
+    block_type _bits = 0, mask = 1;
+    for( unsigned int i = 0; i < walker_type::bits_per_block; ++i ) {
+        if( i & 1 ) {
+            expected.push_back( i );
+
+            _bits |= mask;
+        }
+        mask <<= 1;
+    }
+
+    set_bit_vector_op sbv;
+    other_walker_type::apply( _bits, sbv );
 
     BOOST_REQUIRE_MESSAGE( sbv.indices == expected, "Unexpected list of set bits was returned" );
 }
