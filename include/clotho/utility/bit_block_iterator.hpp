6fe4bf0e3cc5c5e7afaad573669705f90b38ffe2
Patrick Putnam <putnampp@gmail.com>
2014-11-01 14:24:21 -0400

    Corrected template argument lists. Fixed comparison operator definitions to use self_type. Added bit_walking based iterator

--- include/clotho/utility/bit_block_iterator.hpp
+++ include/clotho/utility/bit_block_iterator.hpp
@@ -1,12 +1,15 @@
 #ifndef BIT_BLOCK_ITERATOR_HPP_
 #define BIT_BLOCK_ITERATOR_HPP_
 
+#include <iostream>
 #include <type_traits>
+#include "clotho/utility/bit_walker.hpp"
 
 namespace clotho {
 namespace utility {
 
 struct linear_iterator_tag {};
+struct walk_iterator_tag {};
 
 template < class Block, class Tag = linear_iterator_tag, class Enabled = void >
 class bit_block_iterator;
@@ -14,6 +17,7 @@ class bit_block_iterator;
 template < class Block >
 class bit_block_iterator < Block, linear_iterator_tag, typename std::enable_if< std::is_integral< Block >::value >::type > {
 public:
+    typedef bit_block_iterator< Block, linear_iterator_tag, void > self_type;
     typedef Block block_type;
 
     bit_block_iterator( block_type b = (block_type)0) :
@@ -23,7 +27,7 @@ public:
             next();
     }
 
-    bit_block_iterator( const bit_block_iterator< Block > & rhs ) :
+    bit_block_iterator( const self_type & rhs ) :
         m_val( rhs.m_val )
         , m_index( rhs.m_index ) {
     }
@@ -44,11 +48,11 @@ public:
         return m_index;
     }
 
-    bool operator==( const bit_block_iterator< Block > & rhs ) const {
+    bool operator==( const self_type & rhs ) const {
         return (this->m_val == rhs.m_val);
     }
 
-    bool operator!=(const bit_block_iterator< Block > & rhs ) const {
+    bool operator!=(const self_type & rhs ) const {
         return (this->m_val != rhs.m_val);
     }
 
@@ -66,6 +70,76 @@ protected:
     unsigned int m_index;
 };
 
+template < class Block >
+class bit_block_iterator < Block, walk_iterator_tag, typename std::enable_if< std::is_integral< Block >::value >::type > {
+public:
+    typedef bit_block_iterator< Block, walk_iterator_tag, void > self_type;
+    typedef Block block_type;
+
+    bit_block_iterator( block_type b = (block_type)0) :
+        m_val(b)
+        , m_index(0)
+        , m_base_index(0)
+        , m_shift_next(0) {
+        next();
+    }
+
+    bit_block_iterator( const self_type & rhs ) :
+        m_val( rhs.m_val )
+        , m_index( rhs.m_index )
+        , m_base_index( rhs.m_base_index )
+        , m_shift_next(rhs.m_shift_next) {
+    }
+
+    bit_block_iterator & operator++() {
+        m_base_index += m_shift_next;
+        m_val >>= m_shift_next;
+        next();
+        return *this;
+    }
+
+    bit_block_iterator operator++(int) {
+        bit_block_iterator res(*this);
+        this->operator++();
+        return res;
+    }
+
+    unsigned int operator*() const {
+        return m_index;
+    }
+
+    bool operator==( const self_type & rhs ) const {
+        return (this->m_val == rhs.m_val);
+    }
+
+    bool operator!=(const self_type & rhs ) const {
+        return (this->m_val != rhs.m_val);
+    }
+
+    virtual ~bit_block_iterator() {}
+
+protected:
+    typedef unsigned short sub_block_type;
+    typedef clotho::utility::bit_block_walker< sizeof( sub_block_type ) * 8 > walker_type;
+
+    inline void next() {
+        if( m_val ) {
+            sub_block_type v = (sub_block_type) m_val;
+            while(!v) {
+                m_val >>= walker_type::bits_per_block;
+                m_base_index += walker_type::bits_per_block;
+                v = (sub_block_type) m_val;
+            }
+            set_bit_node * n = walker_type::getNode() + v;
+
+            m_index = m_base_index + n->bit_index;
+            m_shift_next =  n->bit_shift_next;
+        }
+    }
+
+    block_type m_val;
+    unsigned int m_index, m_base_index, m_shift_next;
+};
 }   // namespace utility {
 }   // namespace clotho {
 
