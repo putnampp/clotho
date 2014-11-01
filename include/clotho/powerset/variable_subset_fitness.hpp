7e2f96569f82fcfabbafd3da7a1832456ec77233
Patrick Putnam <putnampp@gmail.com>
2014-11-01 14:20:06 -0400

    Added in free_list masking elements

--- include/clotho/powerset/variable_subset_fitness.hpp
+++ include/clotho/powerset/variable_subset_fitness.hpp
@@ -28,7 +28,8 @@ public:
         typename subset_type::cblock_iterator base_it, base_end;
         typename subset_type::cblock_iterator alt_it, alt_end;
 
-        typename subset_type::powerset_type::cvariable_iterator elem_it;
+        typename subset_type::powerset_type::cvariable_iterator elem_it, elem_end;
+        typename subset_type::powerset_type::cfree_block_iterator free_it, free_end;
 
         if( !base ) {
             // base sequence is empty
@@ -42,6 +43,10 @@ public:
             base_end = alt->end();
 
             elem_it = alt->getParent()->variable_begin();
+            elem_end = alt->getParent()->variable_end();
+
+            free_it = alt->getParent()->free_begin();
+            free_end = alt->getParent()->free_end();
         } else if( !alt ) {
             // alt sequence is empty
             base_it = base->begin();
@@ -53,6 +58,10 @@ public:
             alt_end = base->end();
 
             elem_it = base->getParent()->variable_begin();
+            elem_end = base->getParent()->variable_end();
+
+            free_it = base->getParent()->free_begin();
+            free_end = base->getParent()->free_end();
         } else {
             assert( base->isSameFamily( alt ) );
 
@@ -63,16 +72,21 @@ public:
             alt_end = alt->end();
 
             elem_it = base->getParent()->variable_begin();
+            elem_end = base->getParent()->variable_end();
+
+            free_it = base->getParent()->free_begin();
+            free_end = base->getParent()->free_end();
         }
 
         fitness_type       bfitness;
 
-        block_type mask = (block_type) -1;
         result_type res = f;
         while( true ) {
             if( alt_it == alt_end ) {
                 while( base_it != base_end ) {
+                    assert( free_it != free_end && elem_it != elem_end );
                     block_type _base = (*base_it++);
+                    block_type mask = ~(*free_it++);
                     res = bfitness( res, _base, (block_type)0, mask, elem_it );
                     elem_it += bits_per_block;
                 }
@@ -81,14 +95,18 @@ public:
 
             if( base_it == base_end ) {
                 while( alt_it != alt_end ) {
+                    assert( free_it != free_end && elem_it != elem_end );
                     block_type _alt = (*alt_it++);
+                    block_type mask = ~(*free_it++);
                     res = bfitness(res, (block_type)0, _alt, mask, elem_it );
                     elem_it += bits_per_block;
                 }
                 break;
             }
 
+            assert( free_it != free_end && elem_it != elem_end );
             block_type _base = (*base_it++), _alt = (*alt_it++);
+            block_type mask = ~(*free_it++);
             res = bfitness(res, _base, _alt, mask, elem_it );
             elem_it += bits_per_block;
         }
