b2cc18f7cc27003aa5f312dc0ca650ec48101dd6
Patrick Putnam <putnampp@gmail.com>
2014-11-04 15:49:35 -0500

    created standalone file for no fit

---
+++ include/clotho/fitness/no_fit.hpp
@@ -0,0 +1,12 @@
+#ifndef CLOTHO_NO_FIT_HPP_
+#define CLOTHO_NO_FIT_HPP_
+
+namespace clotho {
+namespace fitness {
+
+struct no_fit {};
+
+}   // namespace fitness {
+}   // namespace clotho {
+
+#endif  // CLOTHO_NO_FIT_HPP_
