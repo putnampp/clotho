f814e7f05d27eb8aa210223e1c7a8f231f611d53
Patrick Putnam <putnampp@gmail.com>
2014-11-06 10:20:25 -0500

    Added an iterator which preprocesses the block and fills an array with the indices of the set bits; Empirically slower performance

--- include/clotho/utility/bit_block_iterator.hpp
+++ include/clotho/utility/bit_block_iterator.hpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <type_traits>
+#include <cstring>
+
 #include "clotho/utility/bit_walker.hpp"
 
 namespace clotho {
@@ -10,6 +12,7 @@ namespace utility {
 
 struct linear_iterator_tag {};
 struct walk_iterator_tag {};
+struct preprocess_iterator_tag {};
 
 template < class Block, class Tag = linear_iterator_tag, class Enabled = void >
 class bit_block_iterator;
@@ -42,6 +45,10 @@ public:
         bit_block_iterator res(*this);
         this->operator++();
         return res;
+    }
+
+    inline bool done() const {
+        return (m_val == 0);
     }
 
     unsigned int operator*() const {
@@ -104,6 +111,10 @@ public:
         return res;
     }
 
+    inline bool done() const {
+        return (m_val == 0);
+    }
+
     unsigned int operator*() const {
         return m_index;
     }
@@ -149,6 +160,101 @@ protected:
     block_type m_val;
     unsigned int m_index, m_base_index, m_shift_next;
 };
+
+
+template < class Block >
+class bit_block_iterator < Block, preprocess_iterator_tag, typename std::enable_if< std::is_integral< Block >::value >::type > {
+public:
+    typedef bit_block_iterator< Block, preprocess_iterator_tag, void > self_type;
+    typedef Block block_type;
+
+    static const unsigned int bits_per_block = sizeof( block_type ) * 8;
+
+    bit_block_iterator( block_type b = (block_type)0) :
+        m_val(b)
+        , m_index(0)
+        , m_max(0)
+    {
+        fill();
+    }
+
+    bit_block_iterator( const self_type & rhs ) :
+        m_val( rhs.m_val )
+        , m_index(0)
+        , m_max(0)
+    {
+        memcpy( m_indices, rhs.m_indices, sizeof( unsigned int) * bits_per_block);
+    }
+
+    bit_block_iterator & operator++() {
+        if( m_index < m_max ) {
+            ++m_index;
+        }
+        return *this;
+    }
+
+    bit_block_iterator operator++(int) {
+        bit_block_iterator res(*this);
+        this->operator++();
+        return res;
+    }
+
+    inline bool    done() const {
+        return (m_index >= m_max );
+    }
+
+    unsigned int operator*() const {
+        return ((m_index < m_max ) ? m_indices[m_index] : bits_per_block);
+    }
+
+    bool operator==( const self_type & rhs ) const {
+        return (this->m_indices == rhs.m_indices);
+    }
+
+    bool operator!=(const self_type & rhs ) const {
+        return (this->m_indices != rhs.m_indices );
+    }
+
+    void reset( block_type b ) {
+        m_val = b;
+        m_index = 0;
+        m_max = 0;
+        fill();
+    }
+
+    virtual ~bit_block_iterator() {}
+
+protected:
+    typedef unsigned short sub_block_type;
+    typedef clotho::utility::bit_block_walker< sizeof( sub_block_type ) * 8 > walker_type;
+
+    inline void fill() {
+        if( m_val ) {
+            block_type tmp_val = m_val;
+            unsigned int base_index = 0;
+            do {
+                sub_block_type v = (sub_block_type) tmp_val;
+                while(!v) {
+                    tmp_val >>= walker_type::bits_per_block;
+                    base_index += walker_type::bits_per_block;
+                    v = (sub_block_type) tmp_val;
+                }
+                set_bit_node * n = walker_type::getNode() + v;
+
+                m_indices[m_max++] = base_index + n->bit_index;
+                tmp_val >>= n->bit_shift_next;
+                base_index += n->bit_shift_next;
+            } while( tmp_val );
+        }
+    }
+
+    block_type m_val;
+
+    unsigned int m_index, m_max;
+
+    unsigned int m_indices[ bits_per_block ];
+};
+
 }   // namespace utility {
 }   // namespace clotho {
 
