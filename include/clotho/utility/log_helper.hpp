e393039751e3c6b1d0e3a75725c9c54d204a1047
Patrick Putnam <putnampp@gmail.com>
2014-12-04 14:32:48 -0500

    Added ability add vectors to arrays (ie. table of values). Made part of utility namespace

--- include/clotho/utility/log_helper.hpp
+++ include/clotho/utility/log_helper.hpp
@@ -5,6 +5,10 @@
 #include "clotho/utility/timer.hpp"
 
 #include <string>
+#include <vector>
+
+namespace clotho {
+namespace utility {
 
 template < class Value >
 void add_value_array( boost::property_tree::ptree & array, const Value & t ) {
@@ -24,6 +28,18 @@ void add_value_array( boost::property_tree::ptree & array, const std::pair< A, B
     c.push_back (std::make_pair( "", b ) );
     array.push_back( std::make_pair("", c ) );
 }
+
+template < class A >
+void add_value_array( boost::property_tree::ptree & array, const std::vector< A > & t ) {
+    boost::property_tree::ptree n;
+    typename std::vector< A >::const_iterator it = t.begin();
+    while( it != t.end() ) {
+        add_value_array( n, (*it));
+        ++it;
+    }
+
+    array.push_back( std::make_pair("", n));
+};
 
 inline void add_value_array( boost::property_tree::ptree & array, const clotho::utility::timer & t ) {
     boost::property_tree::ptree node;
@@ -51,5 +67,8 @@ void add_node( boost::property_tree::ptree & r, const std::string & path, const 
     oss << ind.second << ": " << (*ind.second);
     r.put( path + ".second", oss.str() );
 }
+
+}   // namespace utility
+}   // namespace clotho
 
 #endif  // LOG_HELPER_HPP_
