c58cdf6be29ef4f3db730124204a821ea53f7efa
Patrick Putnam <putnampp@gmail.com>
2014-12-26 13:50:31 -0500

    Initial commit

---
+++ include/clotho/genetics/constant_fitness_metric.hpp
@@ -0,0 +1,29 @@
+#ifndef CONSTANT_FITNESS_METRIC_HPP_
+#define CONSTANT_FITNESS_METRIC_HPP_
+
+class constant_fitness_metric {
+public:
+    typedef double      real_type;
+    typedef real_type   result_type;
+
+    constant_fitness_metric( real_type c = 1. ) :
+        m_val( c )
+    {}
+
+    result_type operator()() {
+        return m_val;
+    }
+
+    result_type operator()( real_type x ) {
+        return m_val;
+    }
+
+    result_type operator()( const std::vector< real_type > & multi_variate ) {
+        return m_val;
+    }
+
+protected:
+    real_type m_val;
+};
+
+#endif  // CONSTANT_FITNESS_METRIC_HPP_
