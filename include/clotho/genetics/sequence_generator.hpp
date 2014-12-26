75aedf524be6c0c748df07d43d20eec7ea1a30bf
Patrick Putnam <putnampp@gmail.com>
2014-12-26 14:08:00 -0500

    Changed to utilize same empty set for all generated empty sequences

--- include/clotho/genetics/sequence_generator.hpp
+++ include/clotho/genetics/sequence_generator.hpp
@@ -14,16 +14,18 @@ public:
     typedef typename sequence_type::powerset_type               set_type;
     typedef sequence_generator< result_type >                   self_type;
 
-    sequence_generator( set_type & s ) : m_set( &s ) {}
-    sequence_generator( const self_type & s ) : m_set( s.m_set ) {}
+    sequence_generator( set_type & s ) : m_set( &s ), m_res( s.create_subset() ) {}
+    sequence_generator( const self_type & s ) : m_set( s.m_set ), m_res( s.m_res ) {}
 
     result_type operator()( ) {
-        return m_set->create_subset();
+//        return m_set->create_subset();
+        return m_res;
     }
 
     virtual ~sequence_generator() {}
 protected:
     set_type    * m_set;
+    result_type m_res;
 };
 
 // sequence_generator< Sequence, sequence_generator< Sequence, recombine > >;
