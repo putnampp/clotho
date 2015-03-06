50e462621d8abc05d9bd0bbead5cdf70a0ad7ace
Patrick Putnam <putnampp@gmail.com>
2015-03-06 11:23:10 -0500

    Represents original iterator based approach

---
+++ include/clotho/recombination/iterate_bit_block_recombiner.hpp
@@ -0,0 +1,53 @@
+#ifndef ITERATE_BIT_BLOCK_RECOMBINER_HPP_
+#define ITERATE_BIT_BLOCK_RECOMBINER_HPP_
+
+#include "clotho/recombination/bit_block_recombiner_def.hpp"
+#include "clotho/recombination/inspect_methods.hpp"
+
+namespace clotho {
+namespace recombine {
+namespace walker {
+namespace tag {
+
+struct iterate_and_classify {};
+
+}   // namespace tag
+}   // namespace walker
+}   // namespace recombine
+}   // namespace clotho
+
+namespace clotho {
+namespace recombine {
+
+template < class Classifier, class InspectMethodTag >
+class bit_block_recombiner< Classifier, InspectMethodTag, clotho::recombine::walker::tag::iterate_and_classify > {
+public:
+    typedef Classifier classifier_type;
+
+    bit_block_recombiner( const classifier_type & cfier ) : m_cfier( cfier ) {}
+
+    template < class Block, class ElementIterator >
+    Block operator()( const Block b0, const Block b1, const ElementIterator first ) {
+        typedef clotho::utility::bit_block_iterator< Block, clotho::utility::tag::linear_iterator_tag > iterator;
+
+        Block mask = (Block)0;
+        iterator bit_it( InspectMethodTag::select(b0, b1) ), bit_end;
+        while( bit_it != bit_end ) {
+            unsigned int idx = (*bit_it++);
+            if( m_cfier( *(first + idx) ) ) {
+                mask |= ((Block)1 << idx );
+            }
+        }
+
+        Block res = ((b0 & mask) | (b1 & ~ mask) );
+        return res;
+
+    }
+
+protected:
+    classifier_type m_cfier;
+};
+
+}   // namespace recombine
+}   // namespace clotho
+#endif  // ITERATE_BIT_BLOCK_RECOMBINER_HPP_
