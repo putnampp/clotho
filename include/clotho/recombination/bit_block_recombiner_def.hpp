f2232e474416d3a17e260ca384082c2d0e33c41a
Patrick Putnam <putnampp@gmail.com>
2015-03-06 11:20:51 -0500

    Parsing bit_block_recombiner into multiple types

---
+++ include/clotho/recombination/bit_block_recombiner_def.hpp
@@ -0,0 +1,13 @@
+#ifndef BIT_BLOCK_RECOMBINER_DEF_HPP_
+#define BIT_BLOCK_RECOMBINER_DEF_HPP_
+
+namespace clotho {
+namespace recombine {
+
+template < class Classifier, class InspectMethodTag, class WalkMethodTag >
+class bit_block_recombiner;
+
+}   // namespace clotho {
+}   // namespace recombine {
+
+#endif  // BIT_BLOCK_RECOMBINER_DEF_HPP_
