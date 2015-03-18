b0c170c9f20231cf9867ae8e1b578800caae5739
Patrick Putnam <putnampp@gmail.com>
2015-03-18 10:54:14 -0400

    Added age property

--- include/clotho/genetics/individual_reproduction.hpp
+++ include/clotho/genetics/individual_reproduction.hpp
@@ -32,14 +32,14 @@ public:
         , m_rec_gen( rec ) {
     }
 
-    result_type operator()( individual_type & p0, individual_type & p1 ) {
-        mutation_type   mut0 = m_mut_gen();
+    result_type operator()( individual_type & p0, individual_type & p1, unsigned int gen = 0 ) {
+        mutation_type   mut0 = m_mut_gen( gen );
         recombination_type rec0 = m_rec_gen();
 
         sequence_type   c0 = rec0( p0, mut0.event_count() == 0 );
         mut0( c0 );
 
-        mutation_type mut1 = m_mut_gen();
+        mutation_type mut1 = m_mut_gen( gen );
         recombination_type rec1 = m_rec_gen();
 
         sequence_type   c1 = rec1( p1, mut1.event_count() == 0 );
