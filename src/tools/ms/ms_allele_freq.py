01947354d8d99a910242d4232827ac22d1daccea
Patrick Putnam <putnampp@gmail.com>
2015-03-25 15:27:39 -0400

    Added ability to output distributions as comma separated value (CSV) tables for GNUPlot

--- src/tools/ms/ms_allele_freq.py
+++ src/tools/ms/ms_allele_freq.py
@@ -12,6 +12,7 @@ from pprint import pprint
 
 parser = OptionParser()
 parser.add_option("-i", "--input", dest="file_path", help="Input MS formatted file" )
+parser.add_option("-g", "--gnu", dest="gnu_path", help="Path GNU Plot format")
 
 (options, args) = parser.parse_args()
 
@@ -29,11 +30,19 @@ distributions = {}
 allele_dist = []
 seq_dist = {}
 
+# parse header to first empty line
+for line in f:
+    line=line.rstrip('\n')
+    if len(line) == 0:
+        break
+
+in_sample=False
 nSites = 0 
 for line in f:
     line=line.rstrip('\n')
     if line.startswith('//'):
         nReps+=1
+        in_sample=True
     elif len(line) == 0:
         distributions[nReps] = {}
         distributions[nReps]['allele_num'] = { x:y for x,y in enumerate(allele_dist) if y != 0 }
@@ -49,6 +58,7 @@ for line in f:
         nSeqs=0
         seq_dist = {}
         allele_dist = []
+        in_sample=False
     elif ss_regex.match( line ):
         nSites=int(ss_regex.findall(line)[0])
         allele_dist = [0] * nSites
@@ -66,6 +76,47 @@ for line in f:
             seq_dist[n] = 1
 f.close()
 
+if in_sample:
+    distributions[nReps] = {}
+    distributions[nReps]['allele_num'] = { x:y for x,y in enumerate(allele_dist) if y != 0 }
+    distributions[nReps]['seq_dist'] = seq_dist
+    distributions[nReps]['sequence_count'] = nSeqs
+
+    tmp = [0] * nSeqs
+    for y in allele_dist:
+        tmp[y] += 1
+
+    distributions[nReps]['allele_dist'] = { x:y for x,y in enumerate(tmp) if y != 0}
+
 print nReps
 
-pprint(distributions)
+#pprint(distributions)
+
+if len( options.gnu_path ) != 0:
+    for x,y in distributions.iteritems():
+        # x = repeat
+        # y = {...}
+        print x
+        if 'allele_num' in y:
+            path = options.gnu_path + ".allele_num." + str(x) + ".csv"
+            w=open( path, 'w')
+            w.write( "#Allele,Count\n")
+            for a,b in sorted(y['allele_num'].iteritems()):
+                w.write( "%d,%d\n" % (a, b))
+            w.close()
+
+        if 'seq_dist' in y:
+            path = options.gnu_path + ".seq_dist." + str(x) + ".csv"
+            w=open( path, 'w')
+            w.write( "#Allele Count,Sequence Count\n")
+            for a,b in sorted(y['seq_dist'].iteritems()):
+                w.write( "%d,%d\n" % (a, b))
+            w.close()
+
+        if 'allele_dist' in y:
+            path = options.gnu_path + ".allele_dist." + str(x) + ".csv"
+            w=open( path, 'w')
+            w.write( "#Sequence Count,Alleles\n")
+            for a,b in sorted(y['allele_dist'].iteritems()):
+                w.write( "%d,%d\n" % (a, b))
+            w.close()
