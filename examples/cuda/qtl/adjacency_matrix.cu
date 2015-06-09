12295121153d12da198f64657f63f695104d3517
Patrick Putnam <putnampp@gmail.com>
2015-06-09 15:59:55 -0400

    Increased row width to be match device cacheline of 128-bytes; Added indexing for metadata pages

--- examples/cuda/qtl/adjacency_matrix.cu
+++ examples/cuda/qtl/adjacency_matrix.cu
@@ -13,20 +13,21 @@
 //   limitations under the License.
 #include "adjacency_matrix.h"
 
+#include <iostream>
 
-
-
-// c / (COLUMNS_PER_PAGE) = column pages
-// r / (ROWS_PER_PAGE) = row pages
 adjacency_matrix::adjacency_matrix( unsigned int rows, unsigned int cols ) :
-    page_manager( (rows / MAX_ROW_NODES + 1) * (cols / MAX_COLUMN_NODES + 1))
-    , m_rows( rows )
-    , m_cols( cols )
-    , m_row_pages( rows / MAX_ROW_NODES + 1)
-    , m_col_pages( cols / MAX_COLUMN_NODES + 1)
-    , m_column_metadata(NULL)
-    , m_row_metadata(NULL)
+    page_manager( 0 )
+    , m_rows( 0 )
+    , m_cols( 0 )
+    , m_row_pages( 0 )
+    , m_col_pages( 0 )
+    , m_col_meta_size(0)
+    , m_row_meta_size(0)
+    , m_size(0)
+    , m_metadata_pages(NULL)
+    , m_data_pages(NULL)
 {
+    resize( rows, cols );
 }
 
 size_t adjacency_matrix::free_pages_count() const {
@@ -41,28 +42,108 @@ size_t adjacency_matrix::column_padding() const {
     return m_col_pages * MAX_COLUMN_NODES - m_cols;
 }
 
+size_t adjacency_matrix::page_size() const {
+    return m_col_meta_size + m_row_meta_size + m_row_pages * m_col_pages;
+}
+
 void adjacency_matrix::resize( unsigned int rows, unsigned int cols ) {
     if( rows < m_row_pages && cols < m_col_pages ) return;
 
-    unsigned int rpg = rows / MAX_ROW_NODES + 1;
-    unsigned int cpg = cols / MAX_COLUMN_NODES + 1;
+    unsigned int rpg = rows / MAX_ROW_NODES + (( rows % MAX_ROW_NODES ) ? 1 : 0 );
+    unsigned int cpg = cols / MAX_COLUMN_NODES + (( cols % MAX_COLUMN_NODES ) ? 1 : 0 );
 
-    if( rpg * cpg > capacity() ) {
-        reserve( rpg * cpg );
-    }
+    unsigned int meta_cols = cpg * MAX_COLUMN_NODES;
+    unsigned int meta_rows = rpg * MAX_ROW_NODES;
+
+    unsigned int mcpg = meta_cols / MAX_NODE_PER_PAGE + ((meta_cols % MAX_NODE_PER_PAGE ) ? 1 : 0);
+    unsigned int mrpg = meta_rows / MAX_NODE_PER_PAGE + ((meta_rows % MAX_NODE_PER_PAGE ) ? 1 : 0);
+
+    unsigned int dpg = rpg*cpg;
+    unsigned int mpg = mcpg + mrpg;
+    unsigned int totpages = mpg + dpg;
+
+    reserve( totpages );
 
     //TODO: need to transform current matrix space
+    // Current algorithm re-maps all pages
 
+    if( mpg > m_col_meta_size + m_row_meta_size ) {
+        if( m_metadata_pages ) {
+            free( m_metadata_pages );
+
+            cudaError_t err = cudaFree(m_device_metadata);
+            if( err != cudaSuccess ) {
+                std::cerr << "Unable to free device metadata\n";
+            }
+        }
+        m_metadata_pages = (page_type *) malloc( sizeof(page_type) * mpg);
+        cudaError_t err = cudaMalloc(&m_device_metadata, sizeof(page_type)*mpg);
+        if( err != cudaSuccess ) {
+            std::cerr << "Unable to allocate device metadata\n";
+        }
+    }
+
+    if( dpg > m_row_pages * m_col_pages ) {
+        if( m_data_pages ) {
+            free( m_data_pages );
+            
+            cudaError_t err =  cudaFree( m_device_data);
+            if( err != cudaSuccess ) {
+                std::cerr << "Unable to free device data\n";
+            }
+        }
+
+        m_data_pages = (page_type *)malloc( sizeof(page_type) * dpg);
+        cudaError_t err = cudaMalloc(&m_device_data, sizeof(page_type) * dpg);
+        if( err != cudaSuccess ) {
+            std::cerr << "Unable to allocate device data\n";
+        }
+    }
+
+    m_size = 0;
+    m_col_meta_size = mcpg;
+    m_row_meta_size = mrpg;
+    memcpy( m_metadata_pages, m_pages + m_size, sizeof(page_type) * mpg);
+    cudaMemcpy(m_device_metadata, m_metadata_pages, sizeof(page_type) * mpg, cudaMemcpyHostToDevice);
+
+    m_size += mpg;
     m_row_pages = rpg;
     m_col_pages = cpg;
+    memcpy(m_data_pages, m_pages + m_size, sizeof(page_type) * dpg);
+    cudaMemcpy(m_device_data, m_data_pages, sizeof(page_type) * dpg, cudaMemcpyHostToDevice);
+    m_size += dpg;
+
+    m_rows = rows;
+    m_cols = cols;
 }
 
-adjacency_matrix::~adjacency_matrix() {}
+unsigned int adjacency_matrix::column_metadata_nodes() const {
+    return m_col_meta_size * MAX_NODE_PER_PAGE;
+}
+
+unsigned int adjacency_matrix::row_metadata_nodes() const {
+    return m_row_meta_size * MAX_NODE_PER_PAGE;
+}
+
+adjacency_matrix::~adjacency_matrix() {
+    if( m_metadata_pages ) {
+        free( m_metadata_pages );
+        cudaFree( m_device_metadata );
+    }
+
+    if( m_data_pages ) {
+        free( m_data_pages );
+        cudaFree( m_device_data );       
+    }
+}
 
 std::ostream & operator<<( std::ostream & out, const adjacency_matrix & rhs ) {
     out << "Total Pages: " << rhs.capacity() << " (" << rhs.allocated_size() << " bytes)" << "\n";
-    out << "Page Dimensions: <" << rhs.m_row_pages << ", " << rhs.m_col_pages << ">\n";
+    out << "Data Page Dimensions: <" << rhs.m_row_pages << ", " << rhs.m_col_pages << ">\n";
     out << "Maximum Node Dimensions: <" << rhs.m_row_pages * adjacency_matrix::MAX_ROW_NODES << ", " << rhs.m_col_pages * adjacency_matrix::MAX_COLUMN_NODES << ">\n";
     out << "Minimum Node Dimensions: <" << rhs.m_rows << ", " << rhs.m_cols << ">\n";
+
+    out << "Column Metadata Nodes: " << rhs.column_metadata_nodes() << "\n";
+    out << "Row Metadata Nodes: " << rhs.row_metadata_nodes() << "\n";
     return out;
 }
--- examples/cuda/qtl/adjacency_matrix.h
+++ examples/cuda/qtl/adjacency_matrix.h
@@ -38,18 +38,24 @@
 class adjacency_matrix : public page_manager {
 public:
     static const unsigned int BIT_PER_NODE = 1;
-    static const unsigned int BLOCK_PER_ROW = 4;
+    static const unsigned int BLOCK_PER_ROW = 32;    // (8*BLOCK_PER_ROW) -bits per row
 
     static const unsigned int NODE_PER_BLOCK = sizeof(page_manager::block_type) * 8 / BIT_PER_NODE; // (byte/block)*(bit/byte)/(bit/node) = (node/block)
 
     static const unsigned int MAX_COLUMN_NODES = NODE_PER_BLOCK * BLOCK_PER_ROW;
     static const unsigned int MAX_ROW_NODES = (page_manager::BLOCK_COUNT / BLOCK_PER_ROW);
+    static const unsigned int MAX_NODE_PER_PAGE = page_manager::BLOCK_COUNT * NODE_PER_BLOCK;
 
     adjacency_matrix( unsigned int rows, unsigned int cols );
 
     size_t  free_pages_count()  const;
     size_t  row_padding()    const;
     size_t  column_padding() const;
+
+    size_t  page_size() const;
+
+    unsigned int column_metadata_nodes() const;
+    unsigned int row_metadata_nodes() const;
 
     void resize( unsigned int rows, unsigned int cols );
 
@@ -59,8 +65,16 @@ public:
 protected:
     unsigned int m_rows, m_cols;
     unsigned int m_row_pages, m_col_pages;
+    unsigned int m_col_meta_size, m_row_meta_size;
+    unsigned int m_size;
 
-    page_type * m_column_metadata, * m_row_metadata;
+    // host
+    page_type * m_metadata_pages;
+    page_type * m_data_pages;
+
+    // device
+    page_type * m_device_metadata;
+    page_type * m_device_data;
 };
 
 #endif  // ADJACENCY_MATRIX_H_
