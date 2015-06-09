d7ba28c703438281b14b8887c9d9c0343fe4393c
Patrick Putnam <putnampp@gmail.com>
2015-06-09 10:27:35 -0400

    Initial commit

---
+++ examples/cuda/sandbox/buffered_stream.hpp
@@ -0,0 +1,22 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef BUFFERED_STREAM_HPP_
+#define BUFFERED_STREAM_HPP_
+
+#include "buffered_stream_def.hpp"
+
+#include "basic_buffered_stream.hpp"
+#include "double_buffered_stream.hpp"
+
+#endif  // BUFFERED_STREAM_HPP_
---
+++ examples/cuda/sandbox/buffered_stream_def.hpp
@@ -0,0 +1,35 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef BUFFERED_STREAM_DEF_HPP_
+#define BUFFERED_STREAM_DEF_HPP_
+
+#include "clotho/utility/result_type_of.hpp"
+
+#include "igenerator.hpp"
+
+template < class Generator, class BufferMethod, class Enabled = void >
+class BufferedStream;
+
+namespace clotho {
+namespace utility {
+
+template < class G, class B, class E >
+struct result_type_of< BufferedStream< G, B, E > > {
+    typedef typename BufferedStream< G, B, E >::result_t type;
+};
+
+}   // namespace utility
+}   // namespace utility
+ 
+#endif  // BUFFERED_STREAM_DEF_HPP_
