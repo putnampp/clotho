7016309879ddce704e1aae58ff97adb9fb9ed19f
Patrick Putnam <putnampp@gmail.com>
2015-07-25 11:17:03 -0400

    Simplified code

--- include/clotho/genetics/qtl_region_classifier.hpp
+++ include/clotho/genetics/qtl_region_classifier.hpp
@@ -21,6 +21,10 @@
 #include <boost/random/poisson_distribution.hpp>
 #include <set>
 
+//#include "clotho/utility/log_helper.hpp"
+//#include <boost/property_tree/json_parser.hpp>
+//#include <iostream>
+
 namespace clotho {
 namespace utility {
 
@@ -46,24 +50,74 @@ public:
 
     result_type operator()() {
         typename result_type::param_type p;
+        unsigned int n = ((m_bSkip) ? 0 : m_dist(*m_rng) );
 
-        unsigned int n = ((m_bSkip)? 0 : m_dist( *m_rng ));
+        generate_ordered_list( p, 0.0, 1.0, n );
 
-        if( n ) {
-            std::set< double > positions;
-            while( positions.size() < n ) {
-                positions.insert( m_uniform( *m_rng ) );
-            }
+//        boost::property_tree::ptree l;
+//        add_value_array(l, p.begin(), p.end() );
+//        boost::property_tree::write_json( std::cerr, l );
 
-            qtl_allele::trait_weights coeff;
-            for( std::set< double >::iterator it = positions.begin(); it != positions.end(); ++it ) {
-                p.push_back( qtl_allele( *it, DEFAULT_SELECTION, DEFAULT_DOMINANCE, DEFAULT_NEUTRAL, 0, coeff) );
-            }
-        }
-        return result_type( p );
+        return result_type(p);
     }
 
 protected:
+
+    void method1(typename result_type::param_type & p, unsigned int n) {
+        if( n == 0 ) return;
+
+        std::set< double > positions;
+        while( positions.size() < n ) {
+            positions.insert( m_uniform( *m_rng ) );
+        }
+
+        qtl_allele::trait_weights coeff;
+        for( std::set< double >::iterator it = positions.begin(); it != positions.end(); ++it ) {
+            p.push_back( qtl_allele( *it, DEFAULT_SELECTION, DEFAULT_DOMINANCE, DEFAULT_NEUTRAL, 0, coeff) );
+        }
+    }
+
+/**
+ * Construct a position ordered list of qtl_alleles in linear time
+ * Avoids potential of duplicate positions in the list
+ * Avoids having to sort the list
+ */
+//   [lo, hi)        | n   | r    | val                        | lside           | rside | rec | p
+//   [0, 1)          | 10  | 0.5  | 0.5 * 1 + 0 = 0.5          | (0.5 * 9) = 4   | 5     | l   |
+//   [0, 0.5)        | 4   | 0.3  | 0.3 * (0.5) + 0 = 0.15     | (0.3 * 3) = 0   | 3     | l   |
+//   [0, 0.15)       | 0   | -    | -                          | -               | -     | -   |
+//   [0, 0.5)        | 3   | 0.3  | 0.3 * (0.5) + 0 = 0.15     | (0.3 * 2) = 0   | 2     | r   | 0.15
+//   [0.15, 0.5 )    | 2   | 0.8  | 0.8 * (0.35) +0.15 = 0.43  | (0.8 * 1) = 0   | 1     | l   | 0.15
+//   [0.15, 0.43)    | 0   | -    | -                          | -               | -     | -   | 0.15
+//   [0.43, 0.5)     | 1   | 0.11 | 0.11* (0.43) +0.15 = 0.4377| 0               | 0     | r   | 0.15, 0.43
+//   [0.43, 0.4377)  | 0   | -    | -                          | -               | -     | -   | 0.15, 0.43
+//   [0.43, 0.5)     | 1   | 0.11 | 0.11* (0.43) +0.15 = 0.4377| 0               | 0     | r   | 0.15, 0.43, 0.4377
+//   [0.4377, 0.5)   | 0   | -    | -                          | -               | -     | -   | 0.15, 0.43, 0.4377
+//   [0, 1)          | 10  | 0.5  | 0.5 * 1 + 0 = 0.5          | (0.5 * 9) = 4   | 5     | r   | 0.15, 0.43, 0.4377, 0.5
+//   [0.5, 1)        | 5   | 0.9  | 0.9 * (0.5) + 0.5 = 0.95   | (0.5 * 4) = 2   | 2     | l   | 0.15, 0.43, 0.4377, 0.5
+//   [0.5, 0.95)     | 2   | 0.02 | 0.02 * (0.45) + 0.5 = 0.509| (0.02 * 1 ) = 0 | 1     | l   | 0.15, 0.43, 0.4377, 0.5
+//   [0.5, 0.509)    | 0   | -    | -                          | -               | -     | -   | 0.15, 0.43, 0.4377, 0.5
+//   [0.5, 0.95)     | 2   | 0.02 | 0.02 * (0.45) + 0.5 = 0.509| (0.02 * 1 ) = 0 | 1     | r   | 0.15, 0.43, 0.4377, 0.5, 0.509
+
+    void generate_ordered_list( typename result_type::param_type & p, double lo, double hi, unsigned int n ) {
+        if( n-- == 0 ) return;
+        
+        // m_uniform produces X ~ [0, 1)
+        double r = m_uniform( *m_rng );
+
+        while( r == 0.0 || r == 1.0 ) { r = m_uniform( *m_rng ); }
+
+        double val = r * (hi - lo) + lo;
+
+        unsigned int lside = r * n;
+
+        generate_ordered_list( p, lo, val, lside);
+
+        qtl_allele::trait_weights coeff;
+        p.push_back( qtl_allele( val, DEFAULT_SELECTION, DEFAULT_DOMINANCE, DEFAULT_NEUTRAL, 0, coeff) );
+        
+        generate_ordered_list( p, val, hi, n - lside );
+    }
 
     void parseConfig( boost::property_tree::ptree & config ) {
         std::ostringstream oss;
