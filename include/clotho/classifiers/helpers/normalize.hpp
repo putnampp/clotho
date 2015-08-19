bd9f0bb34fc20cd182904b9d35103c563956e0fc
Patrick Putnam <putnampp@gmail.com>
2015-08-19 18:40:52 -0400

    Initial commit

---
+++ include/clotho/classifiers/helpers/normalize.hpp
@@ -0,0 +1,31 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef NORMALIZE_ARGUMENT_HPP_
+#define NORMALIZE_ARGUMENT_HPP_
+
+namespace clotho {
+namespace classifiers {
+namespace helpers {
+
+template < class E >
+inline double normalize_arg( E const& elem ) {
+    assert( false );
+    return elem;
+}
+
+}   // namespace helpers {
+}   // namespace classifiers {
+}   // namespace clotho {
+
+#endif  // NORMALIZE_ARGUMENT_HPP_
