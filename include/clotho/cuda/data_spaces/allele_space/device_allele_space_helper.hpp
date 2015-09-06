da7af4f15ef2c4fdfab37c66b2b9fc9539c6724e
Patrick Putnam <putnampp@gmail.com>
2015-09-06 07:07:15 -0400

    Initial commit

---
+++ include/clotho/cuda/data_spaces/allele_space/device_allele_space_helper.hpp
@@ -0,0 +1,122 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef DEVICE_ALLELE_SPACE_HELPER_HPP_
+#define DEVICE_ALLELE_SPACE_HELPER_HPP_
+
+template < class DataType >
+__global__ void copy_heap( DataType * d_loc, DataType * d_heap, unsigned int N ) {
+    memcpy( d_loc, d_heap, N * sizeof( DataType ) );
+}
+
+template < class RealType, class IntType, class OrderTag >
+void dump_locations( std::ostream & out, const device_allele_space< RealType, IntType, OrderTag > & rhs ) {
+    if( rhs.locations == NULL || rhs.size == 0 ) {
+        out << "[]";
+        return;
+    }
+
+    typedef device_allele_space< RealType, IntType, OrderTag >  space_type;
+    typedef typename space_type::real_type                      real_type;
+
+    unsigned int N = rhs.size;
+    real_type * locations = new real_type[ N ];
+
+    real_type * dLoc;
+    assert( cudaMalloc( (void **) &dLoc, N * sizeof( real_type ) ) == cudaSuccess );
+
+    copy_heap<<< 1, 1 >>>( dLoc, rhs.locations, N );
+    cudaDeviceSynchronize();
+
+    assert( cudaMemcpy( locations, dLoc, N * sizeof( real_type ), cudaMemcpyDeviceToHost ) == cudaSuccess );
+
+    out << "[" << locations[0];
+
+    for( int i = 1; i < N; ++i ) {
+        out << ",\n" << locations[i];
+    }
+    out << "]";
+
+    cudaFree( dLoc );
+    delete locations;
+}
+
+template < class RealType, class IntType, class OrderTag >
+void dump_free_list( std::ostream & out, const device_allele_space< RealType, IntType, OrderTag > & rhs ) {
+    if( rhs.free_list == NULL || rhs.size == 0 ) {
+        out << "[]";
+        return;
+    }
+
+    typedef typename device_allele_space< RealType, IntType, OrderTag >::int_type int_type;
+    unsigned int N = compute_free_list_size< IntType >( rhs.size );
+    int_type * free_list = new int_type[ N ];
+
+    int_type * d_free;
+    assert( cudaMalloc( (void **) &d_free, N * sizeof( int_type ) ) == cudaSuccess );
+
+    copy_heap<<< 1, 1>>>( d_free, rhs.free_list, N );
+    assert( cudaMemcpy( free_list, d_free, N * sizeof( int_type ), cudaMemcpyDeviceToHost ) == cudaSuccess );
+
+    out << std::hex;
+    out << "[0x" << std::setw(sizeof(int_type) * 2) << std::setfill('0') << free_list[0];
+
+    for( int i = 1; i < N; ++i ) {
+        out << ",0x" << std::setw(sizeof(int_type) * 2) << std::setfill('0') << free_list[i];
+    }
+    out << "]";
+
+    out << std::dec;
+
+    cudaFree( d_free );
+    delete free_list;
+}
+
+template < class RealType, class IntType, class OrderTag >
+void dump_free_count( std::ostream & out, const device_allele_space< RealType, IntType, OrderTag > & rhs ) {
+    out << rhs.free_count;
+}
+
+template < class RealType, class IntType >
+void dump_free_count ( std::ostream & out, const device_allele_space< RealType, IntType, unit_ordered_tag< IntType > > & rhs ) {
+    out << "[" << rhs.free_count[0];
+
+    for( int i = 1; i < unit_ordered_tag< IntType >::OBJECTS_PER_UNIT; ++i ) {
+        out << ",\n" << rhs.free_count[i];
+    }
+    out << "]";
+}
+
+template < class RealType, class IntType, class OrderTag >
+std::ostream & operator<<( std::ostream & out, const device_allele_space< RealType, IntType, OrderTag > & rhs ) {
+    out << "{";
+
+#ifdef DUMP_HEAP_VARIABLES
+    out << "\n\"locations\": "; // << std::hex << rhs.locations;
+    dump_locations( out, rhs );
+
+    out << ",\n\"free_list\": ";
+    dump_free_list( out, rhs );
+    out << ",";
+#endif  // LOG_LEVEL
+
+    out << "\n\"free_count\": ";
+    dump_free_count( out, rhs );
+
+    out << ",\n\"size\": " << rhs.size;
+    out << ",\n\"capacity\": " << rhs.capacity;
+    out << "}";
+    return out;
+}
+
+#endif  // DEVICE_ALLELE_SPACE_HELPER_HPP_
---
+++ include/clotho/cuda/data_spaces/sequence_space/device_sequence_space_helper.hpp
@@ -0,0 +1,72 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef DEVICE_SEQUENCE_SPACE_HELPER_HPP_
+#define DEVICE_SEQUENCE_SPACE_HELPER_HPP_
+
+template < class IntType >
+void dump_data( std::ostream & out, const device_sequence_space< IntType > & space ) {
+    if( space.seq_count == 0 || space.seq_width == 0 ) {
+        out << "[]";
+        return;
+    }
+
+    typedef typename device_sequence_space< IntType >::int_type int_type;
+
+    int_type * dSeq;
+    int_type * seqs = new int_type[ space.size ];
+
+    assert( cudaMalloc( (void **) &dSeq, space.size * sizeof( int_type ) ) == cudaSuccess );
+    copy_heap<<< 1, 1 >>>( dSeq, space.sequences, space.size );
+    cudaDeviceSynchronize();
+
+    assert( cudaMemcpy( seqs, dSeq, space.size * sizeof( int_type ), cudaMemcpyDeviceToHost ) == cudaSuccess );
+
+    int_type * ptr = seqs;
+    out << std::hex;
+    out << "[";
+    for( int i = 0; i < space.seq_count; ++i ) {
+        if( i ) {
+            out << ",\n";
+        }
+        out << "[0x" << std::setw( sizeof(int_type) * 2) << std::setfill('0') << (*ptr++);
+        for( int j = 1; j < space.seq_width; ++j ) {
+            out << ",0x" << std::setw( sizeof(int_type) * 2) << std::setfill('0') << (*ptr++);
+        }
+        out << "]";
+    }
+
+    out << "]";
+
+    cudaFree( dSeq );
+    delete seqs;
+}
+
+template < class IntType >
+std::ostream & operator<<( std::ostream & out, const device_sequence_space< IntType > & space ) {
+    out << "{";
+#ifdef DUMP_HEAP_VARIABLES 
+    out << "\n\"data\": ";
+    dump_data( out, space );
+    out << ",";
+#endif  // DUMP_HEAP_VARIABLES
+
+    out << "\n\"seq_count\": " << space.seq_count;
+    out << ",\n\"seq_width\": " << space.seq_width;
+    out << ",\n\"size\": " << space.size;
+    out << ",\n\"capacity\": " << space.capacity;
+    out << "}";
+
+    return out;
+}
+#endif  // DEVICE_SEQUENCE_SPACE_HELPER_HPP_
