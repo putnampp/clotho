09104475a89ea0501e046b576e120cc5e125f17e
Patrick Putnam <putnampp@gmail.com>
2015-09-10 15:31:07 -0400

    Initial commit

---
+++ include/clotho/cuda/curand_state_pool.hpp
@@ -0,0 +1,60 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef CURAND_STATE_POOL_HPP_
+#define CURAND_STATE_POOL_HPP_
+
+#include <boost/property_tree/ptree.hpp>
+#include "clotho/cuda/curand_helper.hpp"
+
+namespace clotho {
+namespace cuda {
+
+class curand_state_pool {
+public:
+
+    typedef curandState_t       state_type;
+    typedef unsigned long long  seed_type;
+
+    typedef clotho::cuda::curand_helper< state_type > helper_type;
+
+    static curand_state_pool * getInstance();
+
+//    static curand_state_pool * getInstance() {
+//        curand_state_pool inst;
+//        return &inst;
+//    }
+
+    void initialize( boost::property_tree::ptree & config );
+
+    state_type * get_device_states();
+
+    unsigned int get_max_blocks() const;
+    unsigned int get_max_threads() const;
+
+    unsigned int get_total_states() const;
+
+    virtual ~curand_state_pool();
+protected:
+    curand_state_pool();
+
+    state_type  * dStates;
+    seed_type   m_seed;
+
+    unsigned int max_threads, max_blocks;
+};
+
+}   // namespace cuda {
+}   // namespace clotho {
+
+#endif  // CURAND_STATE_POOL_HPP_
---
+++ src/clotho/cuda/curand_state_pool.cu
@@ -0,0 +1,94 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#include "clotho/cuda/curand_state_pool.hpp"
+
+#include "clotho/utility/timer.hpp"
+#include <iostream>
+
+namespace clotho {
+namespace cuda {
+
+curand_state_pool::curand_state_pool() :
+    dStates( NULL )
+    , m_seed(0)
+    , max_threads(32)
+    , max_blocks(1)
+{}
+
+curand_state_pool * curand_state_pool::getInstance() {
+    static curand_state_pool inst;
+    return &inst;
+}
+
+void curand_state_pool::initialize( boost::property_tree::ptree & config ) {
+    if( dStates != NULL ) return;
+
+    boost::property_tree::ptree lconfig;
+
+    if( config.get_child_optional( "state_pool" ) != boost::none ) {
+        lconfig = config.get_child( "state_pool" );
+    }
+
+    if( lconfig.get_child_optional( "seed" ) != boost::none ) {
+        m_seed = lconfig.get< seed_type >( "seed", m_seed );
+    }
+
+    if( m_seed == 0 ) {
+        m_seed = clotho::utility::clock_type::now().time_since_epoch().count();
+        lconfig.put("seed", m_seed );
+    }
+
+    if( lconfig.get_child_optional( "max_blocks" ) == boost::none ) {
+        lconfig.put("max_blocks", max_blocks );
+    } else {
+        max_blocks = lconfig.get< unsigned int >( "max_blocks", max_blocks );
+    }
+
+    if( lconfig.get_child_optional( "max_threads" ) == boost::none) {
+        lconfig.put("max_threads", max_threads );
+    } else {
+        max_threads = lconfig.get< unsigned int >("max_threads", max_threads );
+    }
+
+    config.put_child( "state_pool", lconfig );
+
+    if( max_threads * max_blocks != 0 ) {
+        std::cerr << "Initializing state pool: " << max_blocks << ", " << max_threads << std::endl;
+        helper_type::make_states( dStates, m_seed, max_blocks, max_threads );
+    }
+}
+
+typename curand_state_pool::state_type * curand_state_pool::get_device_states() {
+    return dStates;
+}
+
+unsigned int curand_state_pool::get_max_blocks() const {
+    return max_blocks;
+}
+
+unsigned int curand_state_pool::get_max_threads() const {
+    return max_threads;
+}
+
+unsigned int curand_state_pool::get_total_states() const {
+    return max_threads * max_blocks;
+}
+
+curand_state_pool::~curand_state_pool() {
+    helper_type::cleanup_states( dStates );
+    dStates = NULL;
+}
+
+}   // namespace cuda {
+}   // namespace clotho {
