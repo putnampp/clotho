ae6f3bf44e4d17c9bff3207b52dedc8094ea8a7c
Patrick Putnam <putnampp@gmail.com>
2015-09-14 17:22:10 -0400

    Initial commit

---
+++ include/clotho/cuda/data_spaces/phenotype_space/device_phenotype_space_helper.hpp
@@ -0,0 +1,52 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef DEVICE_PHENOTYPE_SPACE_HELPER_HPP_
+#define DEVICE_PHENOTYPE_SPACE_HELPER_HPP_
+
+#include "clotho/utility/state_object.hpp"
+
+namespace clotho {
+namespace utility {
+
+template < class RealType >
+//void get_state( boost::property_tree::ptree & state, const device_phenotype_space< RealType > & obj ) {
+struct state_getter< device_phenotype_space< RealType > > {
+    void operator()( boost::property_tree::ptree & state, const device_phenotype_space< RealType > & obj ) {
+//        std::cerr << "Phenotype" << std::endl;
+
+        state.put( "size", obj.size );
+        state.put( "capacity", obj.capacity );
+
+        if( obj.size == 0 ) return;
+
+        typedef typename device_phenotype_space< RealType >::real_type real_type;
+
+        real_type * ecount = new real_type[ obj.size ];
+
+        copy_heap_data( ecount, obj.phenotypes, obj.size );
+
+        boost::property_tree::ptree e;
+        for( unsigned int i = 0; i < obj.size; ++i ) {
+            clotho::utility::add_value_array( e, ecount[ i ] );
+        }
+
+        state.add_child( "phenotypes", e );
+
+        delete ecount;
+    }
+};
+
+}   // namespace utility
+}   // namespace clotho
+#endif  // DEVICE_PHENOTYPE_SPACE_HELPER_HPP_
---
+++ include/clotho/cuda/data_spaces/phenotype_space/device_phenotype_space_kernels.hpp
@@ -0,0 +1,55 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef DEVICE_PHENOTYPE_SPACE_KERNELS_HPP_
+#define DEVICE_PHENOTYPE_SPACE_KERNELS_HPP_
+
+#include "clotho/cuda/data_spaces/phenotype_space/device_phenotype_space_def.hpp"
+#include "clotho/cuda/data_spaces/data_space_kernel_api.hpp"
+
+template < class RealType >
+__global__ void _delete_space( device_phenotype_space< RealType > * space ) {
+    typename device_phenotype_space< RealType >::real_type * tmp = space->phenotypes;
+
+    if( tmp ) {
+        delete tmp;
+    }
+}
+
+template < class RealType >
+__device__ void _resize_space_impl( device_phenotype_space< RealType > * space, unsigned int N ) {
+    typedef device_phenotype_space< RealType > space_type;
+
+    if( space->capacity < N ) {
+        typename space_type::real_type * tmp = space->phenotypes;
+        if( tmp ) {
+            delete tmp;
+        }
+
+        space->phenotypes = new typename space_type::real_type[ N ];
+        space->capacity = N;
+    }
+
+    space->size = N;
+}
+
+template < class RealType >
+__global__ void _resize_space( device_phenotype_space< RealType > * space, unsigned int N ) {
+    unsigned int tid = threadIdx.y * blockDim.x + threadIdx.x;
+
+    if( tid == 0 ) {
+        _resize_space_impl( space, N );
+    }
+}
+
+#endif  // DEVICE_PHENOTYPE_SPACE_KERNELS_HPP_
