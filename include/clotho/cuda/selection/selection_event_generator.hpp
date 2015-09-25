65e51bbc98208aed8ef7348303613122f791be0a
Patrick Putnam <putnampp@gmail.com>
2015-09-25 16:16:56 -0400

    Cleaned up commented out code

--- include/clotho/cuda/selection/selection_event_generator.hpp
+++ include/clotho/cuda/selection/selection_event_generator.hpp
@@ -16,9 +16,6 @@
 
 #include <boost/property_tree/ptree.hpp>
 
-//#include <curand.h>
-//#include <curand_kernel.h>
-//#include "clotho/cuda/curand_helper.hpp"
 #include "clotho/cuda/curand_state_pool.hpp"
 
 #include "clotho/cuda/data_spaces/event_space/device_event_space.hpp"
@@ -33,17 +30,10 @@ class SelectionEventGenerator : public clotho::utility::iStateObject {
 public:
     typedef device_event_space< unsigned int, no_order_tag > event_space_type;
 
-//    typedef curandState_t                               state_type;
-//    typedef clotho::cuda::curand_helper< state_type >   helper_type;
-//    typedef typename helper_type::seed_type             seed_type;
-//
     typedef clotho::cuda::curand_state_pool             state_pool_type;
 
 
-    SelectionEventGenerator( boost::property_tree::ptree & config ) /*:
-        m_states( NULL )
-        , m_seed( 0 )*/
-    {
+    SelectionEventGenerator( boost::property_tree::ptree & config ) {
         parse_configuration( config );
         initialize();
     }
@@ -53,7 +43,6 @@ public:
 
         typedef typename PopulationType::allele_space_type allele_space_type;
 
-//        random_select_parents_kernel<<< 1, 32 >>>( m_states
         typename state_pool_type::state_type * s = state_pool_type::getInstance()->get_device_states();
 
         random_select_parents_kernel<<< 1, 32 >>>( s
@@ -71,41 +60,17 @@ public:
     }
 
     virtual ~SelectionEventGenerator() {
-//        helper_type::cleanup_states( m_states );
-
         delete_space( dEvents );
     }
 protected:
 
     void parse_configuration( boost::property_tree::ptree & config ) {
         state_pool_type::getInstance()->initialize( config );
-
-//        boost::property_tree::ptree lconfig;
-//
-//        if( config.get_child_optional( "selection" ) != boost::none ) {
-//            lconfig = config.get_child( "selection" );
-//        }
-
-//        if( lconfig.get_child_optional( "rng.seed" ) != boost::none ) {
-//            m_seed = lconfig.get< seed_type >( "rng.seed", m_seed );
-//        }
-//
-//        if( m_seed == 0 ) {
-//            m_seed = clotho::utility::clock_type::now().time_since_epoch().count();
-//            lconfig.put("rng.seed", m_seed );
-//        }
-
-//        config.put_child( "selection", lconfig );
     }
 
     void initialize() {
         create_space( dEvents );
-
-//        helper_type::make_states( m_states, m_seed, 1, 32 );
     }
-
-//    state_type          * m_states;
-//    seed_type           m_seed;
 
     event_space_type    * dEvents;   
 };
