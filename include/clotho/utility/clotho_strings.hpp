68f422b0959b03795ccc3c1d7a29c40dced2d9e2
Patrick Putnam <putnampp@gmail.com>
2015-10-15 11:49:17 -0400

    Centralized location for common strings used for configuration purposes

---
+++ include/clotho/utility/clotho_strings.hpp
@@ -0,0 +1,31 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef CLOTHO_STRINGS_HPP_
+#define CLOTHO_STRINGS_HPP_
+
+#include <string>
+
+extern const std::string TRAIT_BLOCK_K;
+
+extern const std::string MUT_BLOCK_K;
+extern const std::string MU_K;
+
+extern const std::string REC_BLOCK_K;
+extern const std::string RHO_K;
+
+extern const std::string MAX_K;
+extern const std::string MEAN_K;
+extern const std::string SIGMA_K;
+
+#endif  // CLOTHO_STRINGS_HPP_
