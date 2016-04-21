8d58e618a303521b7b5e0aaeea0f3eaac63d13bf
Patrick Putnam <putnampp@gmail.com>
2016-04-20 23:23:58 -0400

    Adding new selection functionality

---
+++ include/clotho/data_spaces/selection/fitness_selection_generator.hpp
@@ -0,0 +1,93 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef CLOTHO_FITNESS_SELECTION_GENERATOR_HPP_
+#define CLOTHO_FITNESS_SELECTION_GENERATOR_HPP_
+
+#include <boost/property_tree/ptree.hpp>
+
+#include <boost/random/discrete_distribution.hpp>
+
+#include <vector>
+
+#include "clotho/data_spaces/selection/selection_generator.hpp"
+
+namespace clotho {
+namespace genetics {
+
+template < class GeneticSpaceType >
+struct fitness_selection {};
+
+template < class RNG, class GeneticSpaceType >
+class SelectionGenerator< RNG, fitness_selection< GeneticSpaceType > > {
+public:
+    typedef RNG                                                                 random_engine_type;
+    typedef GeneticSpaceType                                                    genetic_space_type;
+    typedef typename genetic_space_type::individual_id_type                     individual_id_type;
+
+    typedef std::vector< std::pair< individual_id_type, individual_id_type > >  mate_pair_vector;
+    typedef typename mate_pair_vector::iterator                                 parent_iterator;
+    typedef typename mate_pair_vector::const_iterator                           const_parent_iterator;
+
+    typedef typename genetic_space_type::fitness_score_type                     fitness_score_type;
+    typedef typename genetic_space_type::fitness_scores                         fitness_scores;
+
+    typedef boost::random::discrete_distribution< individual_id_type, fitness_score_type >  distribution_type;
+
+    SelectionGenerator( random_engine_type * rng, boost::property_tree::ptree & config ) :
+        m_rand( rng )
+    {
+        std::cout << "simple fitness selection" << std::endl;
+    }
+
+    void update( genetic_space_type * parents, unsigned int count ) {
+        typename distribution_type::param_type  param_type;
+        m_dist.param( param_type( parents->fitness_begin(), parents->fitness_end() ) );
+
+        while( count-- ) {
+            individual_id_type  id0 = m_dist( *m_rand );
+            individual_id_type  id1 = m_dist( *m_rand );
+
+            m_pairs.push_back( std::make_pair( id0, id1 ) );
+        }   
+    }
+
+    parent_iterator begin() {
+        return m_pairs.begin();
+    }
+
+    parent_iterator end() {
+        return m_pairs.end();
+    }
+
+    const_parent_iterator begin() const {
+        return m_pairs.begin();
+    }
+
+    const_parent_iterator end() const {
+        return m_pairs.end();
+    }
+
+    virtual ~SelectionGenerator() {}
+
+protected:
+    random_engine_type  * m_rand;
+    mate_pair_vector    m_pairs;
+
+    distribution_type   m_dist;
+};
+
+}   // namespace genetics
+}   // namespace clotho
+
+#endif  // CLOTHO_FITNESS_SELECTION_GENERATOR_HPP_
---
+++ unittest/data_spaces/selection_test.cpp
@@ -0,0 +1,61 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#include <boost/test/unit_test.hpp>
+
+#include "../unittest_config.h"
+
+#include "clotho/data_spaces/allele_space/allele_space.hpp"
+#include "clotho/data_spaces/population_space/genetic_space.hpp"
+#include "clotho/data_spaces/selection/selection.hpp"
+
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+using namespace clotho::genetics;
+
+typedef double  position_type;
+typedef double  weight_type;
+typedef unsigned long long block_type;
+
+typedef boost::random::mt19937                                          random_engine_type;
+typedef qtl_allele_vectorized< position_type, weight_type >             allele_type;
+typedef genetic_space< allele_type, block_type >                        genetic_space_type;
+
+BOOST_AUTO_TEST_SUITE( test_data_space )
+
+BOOST_AUTO_TEST_CASE( selection_generator_test ) {
+    typedef SelectionGenerator< random_engine_type, genetic_space_type >    selection_generator_type;
+    random_engine_type  rand;
+    boost::property_tree::ptree config;
+
+    selection_generator_type sel( &rand, config );
+}
+
+BOOST_AUTO_TEST_CASE( fitness_selection_generator_test ) {
+    typedef SelectionGenerator< random_engine_type, fitness_selection< genetic_space_type > > selection_generator_type;
+    random_engine_type  rand;
+    boost::property_tree::ptree config;
+
+    selection_generator_type sel( &rand, config );
+}
+
+BOOST_AUTO_TEST_CASE( assortative_selection_generator_test ) {
+    typedef SelectionGenerator< random_engine_type, assortative_selection< genetic_space_type > > selection_generator_type;
+    random_engine_type  rand;
+    boost::property_tree::ptree config;
+
+    selection_generator_type sel( &rand, config );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
