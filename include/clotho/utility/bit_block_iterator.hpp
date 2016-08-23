12d9af3cb6968b545ec8f51ca65a9ce3ba6e0bda
Patrick Putnam <putnampp@gmail.com>
2016-08-22 21:24:15 -0400

    Adding bit_block_iterator_def header

--- include/clotho/utility/bit_block_iterator.hpp
+++ include/clotho/utility/bit_block_iterator.hpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <cstring>
 
+#include "clotho/utility/bit_block_iterator_def.hpp"
 #include "clotho/utility/linear_bit_block_iterator.hpp"
 //#include "clotho/utility/walk_bit_block_iterator.hpp"
 //#include "clotho/utility/preprocess_bit_block_iterator.hpp"
