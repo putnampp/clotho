997defc69fc1b1ce4941bc538ace4202bb4aaf8e
Patrick Putnam <putnampp@gmail.com>
2016-09-10 21:21:14 -0400

    Initial commits

---
+++ include/clotho/data_spaces/free_space/free_space_mt.hpp
@@ -0,0 +1,119 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef CLOTHO_FREE_SPACE_MT_HPP_
+#define CLOTHO_FREE_SPACE_MT_HPP_
+
+#include <memory>
+
+#include <boost/asio/io_service.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+
+#include "clotho/data_spaces/free_space/free_space_details.hpp"
+#include "clotho/data_spaces/free_space/free_space_task.hpp"
+
+namespace clotho {
+namespace genetics {
+
+template < class SizeType = unsigned int >
+class FreeSpaceAnalyzerMT : public free_space_details< SizeType > {
+public:
+
+    typedef SizeType            size_type;
+
+    FreeSpaceAnalyzerMT( size_type tc ) :
+        m_thread_count( tc )
+    { }
+
+    template < class SequenceSpaceType >
+    void operator()( SequenceSpaceType & ss ) {
+        typedef typename SequenceSpaceType::block_type block_type;
+
+        this->resize( ss.column_count() );
+        
+        block_type * destF = new block_type[ 2 * ss.block_column_count() ];
+        block_type * destV = destF + ss.block_column_count();
+
+        process_space( ss.begin_row(0), destF, destV, ss.block_row_count(), ss.block_column_count() );
+
+        this->analyze_free_indices( destF, destV, ss.block_column_count(), ss.column_count() );
+
+        delete [] destF;
+    }
+
+    virtual ~FreeSpaceAnalyzerMT() { }
+
+protected:
+
+    template < class BlockType >
+    void process_space( BlockType * source, BlockType * destF, BlockType * destV, unsigned int block_rows, unsigned int block_columns ) {
+        typedef free_space_task< BlockType > task_type;
+
+        if( m_thread_count <= 1 ) {
+            task_type task( source, destF, destV, block_rows, block_columns );
+            task();
+        } else {
+            boost::asio::io_service service;
+            boost::thread_group threads;
+            boost::mutex mutef, mutev;
+
+            {
+                std::unique_ptr< boost::asio::io_service::work > work( new boost::asio::io_service::work( service ) );
+                for( size_type i = 0; i < m_thread_count; ++i )
+                    threads.create_thread( boost::bind( &boost::asio::io_service::run, &service ) );
+
+                unsigned int rpb = block_rows / m_thread_count;
+                rpb += ((block_rows % m_thread_count > 0) ? 1 : 0);
+
+                while( block_rows ) {
+                    unsigned int rows = (( rpb < block_rows ) ? rpb : block_rows );
+                    std::shared_ptr< task_type > t( new task_type( source, destF, destV, rows, block_columns ) );
+                    
+                    service.post( boost::bind( &task_type::evaluate_ts, t, &mutef, &mutev) );
+                    block_rows -= rows;
+                    source += rows * block_columns;
+                }
+            }
+
+            threads.join_all();
+        }
+    }
+
+    size_type  m_thread_count;
+};
+
+}   // namespace genetics
+}   // namespace clotho
+
+namespace clotho {
+namespace utility {
+
+template < class SizeType >
+struct state_getter< clotho::genetics::FreeSpaceAnalyzerMT< SizeType > > {
+    typedef clotho::genetics::FreeSpaceAnalyzerMT< SizeType > object_type;
+
+    void operator()( boost::property_tree::ptree & s, object_type & obj ) {
+        boost::property_tree::ptree fr;
+        clotho::utility::add_value_array( fr, obj.free_begin(), obj.free_end() );
+        
+        s.put_child( "free", fr );
+        s.add( "free_size", obj.free_size() );
+        s.add( "variable_count", obj.variable_count() );
+    }
+};
+
+}   // namespace utility
+}   // namespace clotho
+
+#endif  // CLOTHO_FREE_SPACE_MT_HPP_
---
+++ include/clotho/data_spaces/free_space/free_space_task.hpp
@@ -0,0 +1,103 @@
+//   Copyright 2015 Patrick Putnam
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#ifndef CLOTHO_FREE_SPACE_TASK_HPP_
+#define CLOTHO_FREE_SPACE_TASK_HPP_
+
+#include "clotho/data_spaces/task/task.hpp"
+
+#include <boost/thread/thread.hpp>
+
+namespace clotho {
+namespace genetics {
+
+template < class BlockType >
+class free_space_task : public task {
+public:
+    typedef BlockType   block_type;
+
+    typedef free_space_task< BlockType > self_type;
+
+    free_space_task( block_type * src, block_type * fixed_dest, block_type * var_dest, unsigned int block_rows, unsigned int block_cols ) :
+        m_source( src )
+        , m_fixed_dest( fixed_dest )
+        , m_variable_dest( var_dest )
+        , m_block_rows( block_rows )
+        , m_block_columns( block_cols )
+    {}
+
+    free_space_task( const self_type & other ) :
+        m_source( other.m_source )
+        , m_fixed_dest( other.m_fixed_dest )
+        , m_variable_dest( other.m_variable_dest )
+        , m_block_rows( other.m_block_rows )
+        , m_block_columns( other.m_block_columns )
+    {}
+
+    void evaluate_ts( boost::mutex * flock, boost::mutex * vlock) {
+
+        block_type * tempF = new block_type[ 2 * m_block_columns ];
+        block_type * tempV = tempF + m_block_columns;
+
+        memset( tempF, 0, 2 * m_block_columns * sizeof( block_type ) );
+
+        evaluate( tempF, tempV );
+
+        {
+            // lock destination
+            boost::lock_guard< boost::mutex >( *flock );
+
+            for( unsigned int i = 0; i < m_block_columns; ++i ) {
+                m_fixed_dest[ i ] &= tempF[ i ];
+            }
+        }
+
+        {
+            // lock destination
+            boost::lock_guard< boost::mutex >( *vlock );
+            for( unsigned int i = 0; i < m_block_columns; ++i ) {
+                m_variable_dest[ i ] |= tempV[ i ];
+            }
+        }
+
+        delete [] tempF;
+    }
+
+    void operator()( ) {
+        evaluate( m_fixed_dest, m_variable_dest );
+    }
+
+    void evaluate( block_type * tempF, block_type * tempV ) {
+        BOOST_LOG_TRIVIAL(debug) << "Evaluating Free Space: " << m_block_rows << " x " << m_block_columns;
+        block_type * src = m_source;
+        for( unsigned int i = 0; i < m_block_rows; ++i ) {
+            for( unsigned int j = 0; j < m_block_columns; ++j ) {
+                block_type b = src[ j ];
+                tempF[ j ] &= b;
+                tempV[ j ] |= b;
+            }
+            src += m_block_columns;
+        }
+    }
+
+    virtual ~free_space_task() {}
+
+protected:
+    block_type * m_source, * m_fixed_dest, * m_variable_dest;
+    unsigned int m_block_rows, m_block_columns;
+};
+
+}   // namespace genetics
+}   // namespace clotho
+#endif  // CLOTHO_FREE_SPACE_TASK_HPP_
+
